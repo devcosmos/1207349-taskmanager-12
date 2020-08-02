0bf146eee437c45c5202c666ba637f85c36e51d0
Litvinov Valery <va-litv@yandex.ru>
2020-08-02 16:48:01 +0300

    M2T4 added webpack-dev-server to the project

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+  }
 };
