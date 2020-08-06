f320eb8694d7ffbc2771a9b98ec8053df152c16a
Litvinov Valery <va-litv@yandex.ru>
2020-08-06 17:22:08 +0300

    M3T6 moved getRandomInteger to separate file

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,9 +1,4 @@
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
+import {getRandomInteger} from "../utils.js";
 
 const generateDescription = () => {
   const description = [
