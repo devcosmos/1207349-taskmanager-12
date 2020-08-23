32f7747e8c7ebf5f7f2cf385a53ff8514fddc1ce
Litvinov Valery <va-litv@yandex.ru>
2020-08-23 12:33:13 +0300

    M5T12 Inherited simple components

--- src/view/sortings.js
+++ src/view/sortings.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import AbstractView from "./abstract";
 
 export const createSortingsTemplate = () => {
   return (
@@ -10,24 +10,8 @@ export const createSortingsTemplate = () => {
   );
 };
 
-export default class Sortings {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Sortings extends AbstractView {
   getTemplate() {
     return createSortingsTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
