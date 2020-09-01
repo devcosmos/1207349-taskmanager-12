ca4ce615e4ba4403106637a9985b57f4398b04e3
Litvinov Valery <va-litv@yandex.ru>
2020-09-01 10:19:30 +0300

    M5T13 added data attributes in sortings

--- src/view/sortings.js
+++ src/view/sortings.js
@@ -1,11 +1,12 @@
 import AbstractView from "./abstract";
+import {SORT_TYPE} from "../const";
 
 const createSortingsTemplate = () => {
   return (
     `<div class="board__filter-list">
-      <a href="#" class="board__filter">SORT BY DEFAULT</a>
-      <a href="#" class="board__filter">SORT BY DATE up</a>
-      <a href="#" class="board__filter">SORT BY DATE down</a>
+      <a href="#" class="board__filter" data-sort-type="${SORT_TYPE.DEFAULT}">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter" data-sort-type="${SORT_TYPE.DATE_UP}">SORT BY DATE up</a>
+      <a href="#" class="board__filter" data-sort-type="${SORT_TYPE.DATE_DOWN}">SORT BY DATE down</a>
     </div>`
   );
 };
