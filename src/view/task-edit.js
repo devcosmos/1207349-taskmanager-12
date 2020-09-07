2f60f952d09a2b977ae75eaddde5d2c7ff625a06
Litvinov Valery <va-litv@yandex.ru>
2020-09-07 10:50:14 +0300

    M6T16 added descriptionInputHandler

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -32,7 +32,7 @@ const createTaskEditDateTemplate = (dueDate, isDueDate = null) => {
             type="text"
             placeholder=""
             name="date"
-            value="${getFormattingDueDate(dueDate)}"
+            value="${dueDate !== null ? getFormattingDueDate(dueDate) : ``}"
           />
         </label>
       </fieldset>` : ``}
@@ -157,6 +157,7 @@ export default class TaskEdit extends AbstractView {
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
     this._dueDateToggleHandler = this._dueDateToggleHandler.bind(this);
     this._repeatingToggleHandler = this._repeatingToggleHandler.bind(this);
+    this._descriptionInputHandler = this._descriptionInputHandler.bind(this);
 
     this._setInnerHandlers();
   }
@@ -165,7 +166,7 @@ export default class TaskEdit extends AbstractView {
     return createTaskEditTemplate(this._data);
   }
   
-  updateData(update) {
+  updateData(update, justDataUpdating) {
     if (!update) {
       return;
     }
@@ -175,6 +176,10 @@ export default class TaskEdit extends AbstractView {
         this._data,
         update
     );
+
+    if (justDataUpdating) {
+      return;
+    }
 
     this.updateElement();
   }
@@ -203,6 +208,9 @@ export default class TaskEdit extends AbstractView {
     this.getElement()
       .querySelector(`.card__repeat-toggle`)
       .addEventListener(`click`, this._repeatingToggleHandler);
+    this.getElement()
+      .querySelector(`.card__text`)
+      .addEventListener(`input`, this._descriptionInputHandler);
   }
 
   _formSubmitHandler(evt) {
@@ -222,6 +230,13 @@ export default class TaskEdit extends AbstractView {
     this.updateData({
       isRepeating: !this._data.isRepeating
     });
+  }
+
+  _descriptionInputHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      description: evt.target.value
+    }, true);
   }
 
   setFormSubmitHandler(callback) {
