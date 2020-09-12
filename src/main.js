e9b45a226aa00dbcd093183bd6b78c9229f84cd3
Litvinov Valery <va-litv@yandex.ru>
2020-09-12 19:06:17 +0300

    M7T20 added tasks model

--- src/main.js
+++ src/main.js
@@ -4,17 +4,20 @@ import FilterView from "./view/filter";
 import {generateTask} from "./mock/task";
 import {generateFilter} from "./mock/filter";
 import {renderElement} from "./utils/render";
-import BoardPresenter from "./presenter/board.js";
+import BoardPresenter from "./presenter/board";
+import TasksModel from "./model/tasks";
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 renderElement(siteHeaderElement, new MenuView(), RenderPosition.BEFOREEND);
 renderElement(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
 
-boardPresenter.init(tasks);
+boardPresenter.init();
