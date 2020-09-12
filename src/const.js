fd09d24a0af946ea69897294f596afb3e2512bd1
Litvinov Valery <va-litv@yandex.ru>
2020-09-12 19:37:02 +0300

    M7T20 created filter type

--- src/const.js
+++ src/const.js
@@ -28,3 +28,12 @@ export const UpdateType = {
   MINOR: `MINOR`,
   MAJOR: `MAJOR`
 };
+
+export const FilterType = {
+  ALL: `all`,
+  OVERDUE: `overdue`,
+  TODAY: `today`,
+  FAVORITES: `favorites`,
+  REPEATING: `repeating`,
+  ARCHIVE: `archive`
+};
