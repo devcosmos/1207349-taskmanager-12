7be244263361a0af3a92680737b59bbb58d21d0a
Litvinov Valery <va-litv@yandex.ru>
2020-09-12 19:05:30 +0300

    M7T20 created new updatedType and UserAction

--- src/const.js
+++ src/const.js
@@ -16,3 +16,15 @@ export const Mode = {
   DEFAULT: `DEFAULT`,
   EDITING: `EDITING`
 };
+
+export const UserAction = {
+  UPDATE_TASK: `UPDATE_TASK`,
+  ADD_TASK: `ADD_TASK`,
+  DELETE_TASK: `DELETE_TASK`
+};
+
+export const UpdateType = {
+  PATCH: `PATCH`,
+  MINOR: `MINOR`,
+  MAJOR: `MAJOR`
+};
